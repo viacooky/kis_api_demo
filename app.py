15063726fae2c66a468ae69dd1ac1b9447831a6a
viacooky <viacooky@qq.com>
2024-04-01 11:45:51 +0800

    feat: 接口测试

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import requests
 import utils
+import json
 
 
 host = 'https://api.kingdee.com'
@@ -134,7 +135,7 @@ def material_list(auth_data: str, access_token: str, gw_addr: str):
     return resp.json()
 
 
-def user_list(auth_data: str, access_token: str, gw_addr: str):
+def user_list(auth_data: str, access_token: str, gw_addr: str, account_db:str):
     """批量查询用户
     https://open.jdy.com/#/files/api/detail?index=3&categrayId=dded94c553614747b2c9b8b49c396aa6&id=da5973be754211ed86f7cfa07425bf5d
 
@@ -146,10 +147,12 @@ def user_list(auth_data: str, access_token: str, gw_addr: str):
 
     # 构建params
     params = {
+        'AccountDB':account_db,
         'CurrentPage': 1,
         'ItemsOfPage': 10,
-        # 'Search':'',
     }
+
+    json_params = json.dumps(params)
 
     # 构建headers
     headers = {
@@ -160,9 +163,12 @@ def user_list(auth_data: str, access_token: str, gw_addr: str):
         'KIS-AuthData': auth_data,
         'KIS-Ver': '1.0',
         'X-GW-Router-Addr': gw_addr,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'
     }
+    
+
     url = f'{host}{path}?access_token={access_token}'
-    resp = requests.post(url, params=params, headers=headers)
+    resp = requests.post(url,headers=headers,json=json_params) # 这里需要post一个json
     return resp.json()
 
 
@@ -178,34 +184,32 @@ if __name__ == '__main__':
 
 
     # ------------- 主动获取授权 -------------
-    data_app_auth = push_app_authorize(client_id=client_id,
-                                       client_secret=client_secret,
-                                       instance_id=instance_id)
+    # data_app_auth = push_app_authorize(client_id=client_id,
+    #                                    client_secret=client_secret,
+    #                                    instance_id=instance_id)
 
-    accountId = data_app_auth['data'][0]['accountId']
-    serviceId = data_app_auth['data'][0]['serviceId']
-    domain = data_app_auth['data'][0]['domain']
-    app_key = data_app_auth['data'][0]['appKey']
-    app_secret = data_app_auth['data'][0]['appSecret']
+    # accountId = data_app_auth['data'][0]['accountId']
+    # serviceId = data_app_auth['data'][0]['serviceId']
+    # domain = data_app_auth['data'][0]['domain']
+    # app_key = data_app_auth['data'][0]['appKey']
+    # app_secret = data_app_auth['data'][0]['appSecret']
 
     # ------------- 主动获取授权 -------------
 
     # ------------- 获取KIS-Authdata -------------
-    data_kingdee_auth = kingdee_auth_token(
-        app_key=app_key, 
-        app_secret=app_secret, 
-        client_id=client_id, 
-        client_secret=client_secret)
+    # data_kingdee_auth = kingdee_auth_token(
+    #     app_key=app_key, 
+    #     app_secret=app_secret, 
+    #     client_id=client_id, 
+    #     client_secret=client_secret)
 
 
-    uid = data_kingdee_auth['data']['uid']
-    app_token = data_kingdee_auth['data']['app-token']
-    access_token = data_kingdee_auth['data']['access_token']
+    # uid = data_kingdee_auth['data']['uid']
+    # app_token = data_kingdee_auth['data']['app-token']
+    # access_token = data_kingdee_auth['data']['access_token']
     # ------------- 获取KIS-Authdata -------------
 
-    # app_token= 'xxx'
-    # access_token = 'xxx'
-    # domain = 'xxx'
+    
 
 
     # ------------- 业务数据接口 -------------
@@ -216,12 +220,15 @@ if __name__ == '__main__':
 
     # ------------- 业务数据接口 -------------
 
+
     data_user_list = user_list(
         auth_data=app_token,
         access_token=access_token,
-        gw_addr=domain)
+        gw_addr=domain,
+        account_db=account_db)
 
+    # 可能是沙箱的数据问题
     # 'errcode': 10201, 'description': '业务接口返回失败结果，失败原因：403-ParseRequestContent:{"AuthData":{"EID":"16140594","Timestamp":"56207-09-26 01:55:35","Source":"","OpenID":0,"IsEncrypt":"N","Method":"kis.APP006992.api.Material.List","Sign":"91c59aa76d860eef4c90fdc05ed6c38f","State":"2375492168968470","Ver":"1.0"},"BizData":}\r\n请求参数转换成JSON对象异常
 
     # ------------- 业务数据接口 -------------
-    pass
+    print()
